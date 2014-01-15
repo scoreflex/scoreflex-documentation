ef1bc0128b5787205d04667ac196518528bbf2cc
Olivier Favre <olivier@yakaz.com>
2014-01-15 16:10:03 +0100

    Add links to sub-pages in folder node pages

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 # vim: sts=4 sw=4 ts=4 et
 
 set -e
-set -v
 
 # Generate toc
 echo "Generating ToC"
@@ -14,7 +13,16 @@ find -type f -name '*.asciidoc' -not -name 'main*' | while read f; do
     if [ -d "${f%.asciidoc}" ]; then
         echo "$f intro"
         # Change file to have only intro
-        grep -v '^include::' "$f" | asciidoc --backend html5 -o "${f%.asciidoc}.html" -
+        # and link to the included files
+        pathBase="$(dirname "${f#./}")"
+        if [ "$pathBase" = "." ]; then
+            idBase=""
+        else
+            idBase="${pathBase//\//-}-"
+        fi
+        awk 'BEGIN { base="'"$pathBase"'/" } /^include::/ { file = base substr($0, 10, length($0)-9-2); while (getline line<file != 0) { if (match(line, /^(=+ )(.*)$/, a) > 0) { level=a[1]; title = a[2]; break } }; close(file); print $0 " #!prefix=\"" level "\",title=\"" title "\""; next }; {print}' "$f" \
+        | sed -r -e "s/^include::(.*)\.asciidoc\[\] #!prefix=\"([^\"]*)\",title=\"(.*)\"$/\2<<$idBase\1,\3>>\n\n--\n--/; :noslash; s/^(=* *<<[^,]*)\//\1-/; t noslash; :end;" \
+        | asciidoc --backend html5 -o "${f%.asciidoc}.html" -
     else
         echo "$f"
         asciidoc --backend html5 -o "${f%.asciidoc}.html" "$f"
