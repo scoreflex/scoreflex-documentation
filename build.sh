23f9c51385f96d2bda921f677033aaf83640bb24
Olivier Favre <olivier@yakaz.com>
2014-01-21 18:56:27 +0100

    Reduce header level so that a page starts with level 0

--- build.sh
+++ build.sh
@@ -24,13 +24,17 @@ find -type f -name '*.asciidoc' -not -name 'main*' | while read f; do
         echo "$f [$id] intro"
         # Change file to have only intro
         # and link to the included files
-        awk 'BEGIN { base="'"$pathBase"'/" } /^include::/ { file = base substr($0, 10, length($0)-9-2); while (getline line<file != 0) { if (match(line, /^(=+ )(.*)$/, a) > 0) { level=a[1]; title = a[2]; break } }; close(file); print $0 " #!prefix=\"" level "\",title=\"" title "\""; next }; {print}' "$f" \
+        cat "$f" \
+        | awk 'BEGIN { base="'"$pathBase"'/" } /^include::/ { file = base substr($0, 10, length($0)-9-2); while (getline line<file != 0) { if (match(line, /^(=+ )(.*)$/, a) > 0) { level=a[1]; title = a[2]; break } }; close(file); print $0 " #!prefix=\"" level "\",title=\"" title "\""; next }; {print}' \
         | sed -r -e "s/^include::(.*)\.asciidoc\[\] #!prefix=\"([^\"]*)\",title=\"(.*)\"$/\2<<$idBase\1,\3>>\n\n--\n--/; :noslash; s/^(=* *<<[^,]*)\//\1-/; t noslash; :end;" \
+        | awk 'BEGIN { remove=-1 }; remove == -1 && /^=+ / { remove = index($0, " ") - 1; }; remove > 0 && /^=+ / { print substr($0, remove); next; }; { print }' \
         | asciidoc --backend html5 -o "$out.pre" -
         ./rewrite.py index.xml "$out.pre" > "$out"
     else
         echo "$f [$id]"
-        asciidoc --backend html5 --attribute toc --attribute disable-javascript -o "$out.pre" "$f"
+        cat "$f" \
+        | awk 'BEGIN { remove=-1 }; remove == -1 && /^=+ / { remove = index($0, " ") - 1; }; remove > 0 && /^=+ / { print substr($0, remove); next; }; { print }' \
+        | asciidoc --backend html5 --attribute toc --attribute disable-javascript -o "$out.pre" -
         ./rewrite.py index.xml "$out.pre" > "$out"
     fi
     rm "$out.pre"
